0c3af8711d2d84528a2ebb9d2e40caab24baebc5
Dannyfsp <femoscopilala@gmail.com>
2023-04-24 15:47:09 +0100

    Deploying to heroku

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
 import app from "./app";
 
-const port: number = Number(process.env.APP_PORT) || 3050;
+const port: number = Number(process.env.PORT) || 3000;
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
